14f68298b1db0dad04a541071d27c46373cb4ea5
Richard Koubek <koubek.richard@gmail.com>
2025-05-28 11:51:25 +0200

    main method

--- ismd-backend-validator/src/main/java/com/dia/IsmdBackendValidator.java
+++ ismd-backend-validator/src/main/java/com/dia/IsmdBackendValidator.java
@@ -1,8 +1,11 @@
 package com.dia;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IsmdBackendValidator {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        SpringApplication.run(IsmdBackendValidator.class, args);
+    }
 }
