b37b2be8e8fb137c4eeed05685cd457526a8ae4e
solodiuc <solodiuc.alex@softopus.cz>
2025-05-30 16:17:02 +0200

    Update OFNBaseModelTest.java

--- ismd-backend-common/src/test/java/com/dia/models/OFNBaseModelTest.java
+++ ismd-backend-common/src/test/java/com/dia/models/OFNBaseModelTest.java
@@ -99,37 +99,86 @@ public class OFNBaseModelTest {
 
         OntProperty nazevProp = ontModel.getOntProperty(NS + LABEL_NAZEV);
         assertNotNull(nazevProp, "Vlastnost 'nazev' by měla existovat");
-        assertEquals(LABEL_NAZEV, nazevProp.getLabel("cs"), "Property 'nazev' by měla mít český popisek");
-        assertTrue(nazevProp.hasDomain(pojemClass), "Property 'nazev' by měla mít doménu TYP_POJEM");
-        assertTrue(nazevProp.hasRange(ontModel.getOntClass(XSD + "string")), "Property 'nazev' by měla mít rozsah XSD:string");
+        assertEquals(LABEL_NAZEV, nazevProp.getLabel("cs"), "Vlastnost 'nazev' by měla mít český popisek");
+        assertTrue(nazevProp.hasDomain(pojemClass), "Vlastnost 'nazev' by měla mít doménu TYP_POJEM");
+        assertTrue(nazevProp.hasRange(ontModel.getOntClass(XSD + "string")), "Vlastnost 'nazev' by měla mít rozsah XSD:string");
 
         OntProperty popisProp = ontModel.getOntProperty(NS + LABEL_POPIS);
         assertNotNull(popisProp, "Vlastnost 'popis' by měla existovat");
-        assertEquals(LABEL_POPIS, popisProp.getLabel("cs"), "Property 'popis' by měla mít český popisek");
-        assertTrue(popisProp.hasDomain(pojemClass), "Property 'popis' by měla mít doménu TYP_POPIS");
-        assertTrue(popisProp.hasRange(ontModel.getOntClass(XSD + "string")), "Property 'popis' by měla mít rozsah XSD:string");
+        assertEquals(LABEL_POPIS, popisProp.getLabel("cs"), "Vlastnost 'popis' by měla mít český popisek");
+        assertTrue(popisProp.hasDomain(pojemClass), "Vlastnost 'popis' by měla mít doménu TYP_POJEM");
+        assertTrue(popisProp.hasRange(ontModel.getOntClass(XSD + "string")), "Vlastnost 'popis' by měla mít rozsah XSD:string");
 
         OntProperty definiceProp = ontModel.getOntProperty(NS + LABEL_DEF);
         assertNotNull(definiceProp, "Vlastnost 'definice' by měla existovat");
-        assertEquals(LABEL_DEF, definiceProp.getLabel("cs"), "Property 'definice' by měla mít český popisek");
-        assertTrue(definiceProp.hasDomain(pojemClass), "Property 'definice' by měla mít doménu TYP_POPIS");
-        assertTrue(definiceProp.hasRange(ontModel.getOntClass(XSD + "string")), "Property 'definice' by měla mít rozsah XSD:string");
+        assertEquals(LABEL_DEF, definiceProp.getLabel("cs"), "Vlastnost 'definice' by měla mít český popisek");
+        assertTrue(definiceProp.hasDomain(pojemClass), "Vlastnost 'definice' by měla mít doménu TYP_POJEM");
+        assertTrue(definiceProp.hasRange(ontModel.getOntClass(XSD + "string")), "Vlastnost 'definice' by měla mít rozsah XSD:string");
 
         OntProperty zdrojProp = ontModel.getOntProperty(NS + LABEL_ZDROJ);
         assertNotNull(zdrojProp, "Vlastnost 'zdroj' by měla existovat");
-        assertEquals(LABEL_ZDROJ, zdrojProp.getLabel("cs"), "Property 'zdroj' by měla mít český popisek");
-        assertTrue(zdrojProp.hasDomain(pojemClass), "Property 'zdroj' by měla mít doménu TYP_POJEM");
-        assertTrue(zdrojProp.hasRange(ontModel.getOntClass(XSD + "anyURI")), "Property 'zdroj' by měla mít rozsah XSD:anyURI");
+        assertEquals(LABEL_ZDROJ, zdrojProp.getLabel("cs"), "Vlastnost 'zdroj' by měla mít český popisek");
+        assertTrue(zdrojProp.hasDomain(pojemClass), "Vlastnost 'zdroj' by měla mít doménu TYP_POJEM");
+        assertTrue(zdrojProp.hasRange(ontModel.getOntClass(XSD + "anyURI")), "Vlastnost 'zdroj' by měla mít rozsah XSD:anyURI");
 
         OntProperty jeSdilenVPpdfProp = ontModel.getOntProperty(NS + LABEL_JE_PPDF);
         assertNotNull(jeSdilenVPpdfProp, "Vlastnost 'jeSdilenVPpdf' by měla existovat");
-        assertTrue(jeSdilenVPpdfProp.hasDomain(pojemClass), "Property 'jeSdilenVPpdf' by měla mít doménu TYP_POJEM");
-        assertTrue(jeSdilenVPpdfProp.hasRange(ontModel.getOntClass(XSD + "boolean")), "Property 'jeSdilenVPpdf' by měla mít rozsah XSD:boolean");
+        assertEquals(LABEL_JE_PPDF, jeSdilenVPpdfProp.getLabel("cs"), "Vlastnost 'jeSdilenvPpdf' by měla mít český popisek");
+        assertTrue(jeSdilenVPpdfProp.hasDomain(pojemClass), "Vlastnost 'jeSdilenVPpdf' by měla mít doménu TYP_POJEM");
+        assertTrue(jeSdilenVPpdfProp.hasRange(ontModel.getOntClass(XSD + "boolean")), "Vlastnost 'jeSdilenVPpdf' by měla mít rozsah XSD:boolean");
+
+        OntProperty agendaProp = ontModel.getOntProperty(NS + LABEL_AGENDA);
+        assertNotNull(agendaProp, "Vlastnost 'agenda' by měla existovat");
+        assertEquals(LABEL_AGENDA, agendaProp.getLabel("cs"), "Vlastnost 'agenda' by měla mít český popisek");
+        assertTrue(agendaProp.hasDomain(pojemClass), "Vlastnost 'agenda' by měla mít doménu TYP_POJEM");
+        assertTrue(agendaProp.hasRange(RDFS.Resource), "Vlastnost 'agenda' by měla mít rozsah RDFS:Resource");
+
+        OntProperty aisProp = ontModel.getOntProperty(NS + LABEL_AIS);
+        assertNotNull(aisProp, "Vlastnost 'ais' by měla existovat");
+        assertEquals(LABEL_AIS, aisProp.getLabel("cs"), "Vlastnost 'ais' by měla mít český popisek");
+        assertTrue(aisProp.hasDomain(pojemClass), "Vlastnost 'ais' by měla mít doménu TYP_POJEM");
+        assertTrue(aisProp.hasRange(RDFS.Resource), "Vlastnost 'ais' by měla mít rozsah RDFS:Resource");
 
         OntProperty ustanoveniProp = ontModel.getOntProperty(NS + LABEL_UDN);
         assertNotNull(ustanoveniProp, "Vlastnost 'ustanoveni' by měla existovat");
-        assertTrue(ustanoveniProp.hasDomain(neverejnyUdajClass), "Property 'ustanoveni' by měla mít doménu TYP_NEVEREJNY_UDAJ");
-        assertTrue(ustanoveniProp.hasRange(RDFS.Resource), "Property 'ustanoveni' by měla mít rozsah RDFS:Resource");
+        assertEquals(LABEL_UDN, ustanoveniProp.getLabel("cs"), "Vlastnost 'ustanoveni' by měla mít český popisek");
+        assertTrue(ustanoveniProp.hasDomain(neverejnyUdajClass), "Vlastnost 'ustanoveni' by měla mít doménu TYP_NEVEREJNY_UDAJ");
+        assertTrue(ustanoveniProp.hasRange(RDFS.Resource), "Vlastnost 'ustanoveni' by měla mít rozsah RDFS:Resource");
+
+        OntProperty definicniOborProp = ontModel.getOntProperty(NS + LABEL_DEF_O);
+        assertNotNull(definicniOborProp, "Vlastnost 'definicniObor' by měla existovat");
+        assertEquals(LABEL_DEF_O, definicniOborProp.getLabel("cs"), "Vlastnost 'definicniObor' by měla mít český popisek");
+
+        OntProperty oborHodnotProp = ontModel.getOntProperty(NS + LABEL_OBOR_HODNOT);
+        assertNotNull(oborHodnotProp, "Vlastnost 'oborHodnot' by měla existovat");
+        assertEquals(LABEL_OBOR_HODNOT, oborHodnotProp.getLabel("cs"), "Vlastnost 'oborHodnot' by měla mít český popisek");
+
+        OntProperty nadrazenaTrida = ontModel.getOntProperty(NS + LABEL_NT);
+        assertNotNull(nadrazenaTrida, "Vlastnost 'nadrazenaTrida' by měla existovat");
+        assertEquals(LABEL_NT, nadrazenaTrida.getLabel("cs"), "Vlastnost 'nadrazenaTrida' by měla mít český popisek");
+
+        OntProperty souvisejiciUstanoveni = ontModel.getOntProperty(NS + LABEL_SUPP);
+        assertNotNull(souvisejiciUstanoveni, "Vlastnost 'souvisejiciUstanoveni' by měla existovat");
+        assertEquals(LABEL_SUPP, souvisejiciUstanoveni.getLabel("cs"), "Vlastnost 'souvisejiciUstanoveni' by měla mít český popisek");
+
+        OntProperty zpusobSdileniProp = ontModel.getOntProperty(NS + LABEL_ZPUSOB_SDILENI);
+        assertNotNull(zpusobSdileniProp, "Vlastnost 'zpusobSdileni' by měla existovat");
+        assertEquals("Způsob sdílení údaje", zpusobSdileniProp.getLabel("cs"), "Vlastnost 'zpusobSdileni' by měla mít český popisek");
+        assertTrue(zpusobSdileniProp.hasDomain(pojemClass), "Vlastnost 'zpusobSdileni' by měla mít doménu TYP_POJEM");
+        assertTrue(zpusobSdileniProp.hasRange(RDFS.Resource), "Vlastnost 'zpusobSdileni' by měla mít rozsah RDFS:Resource");
+
+        OntProperty zpusobZiskaniProp = ontModel.getOntProperty(NS + LABEL_ZPUSOB_ZISKANI);
+        assertNotNull(zpusobZiskaniProp, "Vlastnost 'zpusobZiskani' by měla existovat");
+        assertEquals("Způsob získání údaje", zpusobZiskaniProp.getLabel("cs"), "Vlastnost 'zpusobZiskani' by měla mít český popisek");
+        assertTrue(zpusobZiskaniProp.hasDomain(pojemClass), "Vlastnost 'zpusobZiskani' by měla mít doménu TYP_POJEM");
+        assertTrue(zpusobZiskaniProp.hasRange(RDFS.Resource), "Vlastnost 'zpusobZiskani' by měla mít rozsah RDFS:Resource");
+
+        OntProperty typObsahuProp = ontModel.getOntProperty(NS + LABEL_TYP_OBSAHU);
+        assertNotNull(typObsahuProp, "Vlastnost 'typObsahu' by měla existovat");
+        assertEquals("Typ obsahu údaje", typObsahuProp.getLabel("cs"), "Vlastnost 'typObsahu' by měla mít český popisek");
+        assertTrue(typObsahuProp.hasDomain(pojemClass), "Vlastnost 'typObsahu' by měla mít doménu TYP_POJEM");
+        assertTrue(typObsahuProp.hasRange(RDFS.Resource), "Vlastnost 'typObsahu' by měla mít rozsah RDFS:Resource");
+
     }
 
 }
