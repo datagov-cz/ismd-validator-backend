3058bc6f646b316709226ccc98faa382b68cb27a
Richard Koubek <koubek.richard@gmail.com>
2025-07-17 20:14:21 +0200

    missing constructors for integration tests

--- ismd-backend-validator/src/main/java/com/dia/controller/dto/ValidationResultsDto.java
+++ ismd-backend-validator/src/main/java/com/dia/controller/dto/ValidationResultsDto.java
@@ -1,11 +1,13 @@
 package com.dia.controller.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor(force = true)
 @RequiredArgsConstructor
 public class ValidationResultsDto {
 
