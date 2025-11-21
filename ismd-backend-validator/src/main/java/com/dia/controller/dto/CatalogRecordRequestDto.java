4b62a49eb0f403be84f796f6cf24d1372d4ce13a
Richard Koubek <koubek.richard@gmail.com>
2025-11-21 16:18:15 +0100

    correct dto class

--- ismd-backend-validator/src/main/java/com/dia/controller/dto/CatalogRecordRequestDto.java
+++ ismd-backend-validator/src/main/java/com/dia/controller/dto/CatalogRecordRequestDto.java
@@ -1,10 +1,11 @@
 package com.dia.controller.dto;
 
 import com.dia.validation.ValidationReport;
+import com.dia.validation.ValidationReportDto;
 import lombok.Getter;
 
 @Getter
 public class CatalogRecordRequestDto {
-    private ValidationReport validationReport;
+    private ValidationReportDto validationReport;
     private String ttlContent;
 }
